11ba7ec2826ca03ffd3776c6b3b8548e38a01428
Wai Yan <waiyan.msc.yu12@gmail.com>
2026-03-11 02:59:57 +0900

    upload_in_cloudinary_update_image_url_code

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -17,11 +17,11 @@ class ProductResource extends JsonResource
     {
         // image url ကို absolute url ပြောင်းပေးတယ် can show in frontend  by URL
 
-        $imageUrl = $this->image_url;
+        // $imageUrl = $this->image_url;
 
-        if ($imageUrl && ! str_starts_with($imageUrl, 'http://') && ! str_starts_with($imageUrl, 'https://')) {
-            $imageUrl = Storage::disk('public')->url(ltrim($imageUrl, '/'));
-        }
+        // if ($imageUrl && ! str_starts_with($imageUrl, 'http://') && ! str_starts_with($imageUrl, 'https://')) {
+        //     $imageUrl = Storage::disk('public')->url(ltrim($imageUrl, '/'));
+        // }
         // image_url ကို absolute url ပြောင်းပေးတယ် အတွက် frontend မှာ show လုပ်ရန်  URL လိုအပ်တယ်။ image_url ကို absolute url ပြောင်းပေးခြင်းဖြင့် frontend မှာ image ကိုပြသနိုင်ပါသည်။
 
         return [
@@ -29,7 +29,7 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'price' => $this->price,
-            'image_url' => $imageUrl, // image url ကို absolute url ပြောင်းပေးတယ်
+            'image_url' => $this->image_url, // image url ကို absolute url ပြောင်းပေးတယ် to call browser by https
             'category_name' => $this->category?->name, // category မရှိလည်း error မတက်အောင် null-safe
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
